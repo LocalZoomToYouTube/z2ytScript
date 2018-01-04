e920d249a88c73b7bac4173c6d14cdbca066e890
Dylan <dylan@Dylans-MacBook.local>
2018-01-04 16:27:42 -0500

    added notifier

--- node.js
+++ node.js
@@ -1,5 +1,18 @@
 const fs = require('fs');
+const notifier = require('node-notifier');
 const { spawnSync } = require('child_process');
 
 fs.chmodSync('z2yt.sh', '755');
-const child = spawnSync('sh', ['z2yt.sh'], { stdio: 'inherit', });
+
+notifier.notify({
+  title: 'Zoom To Youtube',
+  message: 'We noticed you recently recorded a zoom video.  Would you like to upload it to youtube?',
+  actions: "Yes",
+  closeLabel: 'Not This Time',
+  reply: true,
+  sound: true
+});
+
+notifier.on('click', (object, options) => {
+  const child = spawnSync('sh', ['z2yt.sh'], { stdio: 'inherit' });
+});
