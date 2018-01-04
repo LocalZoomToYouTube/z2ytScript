8a4deea07a82006d048c2dbda03f724ab2723259
Dylan <dylan@Dylans-MacBook.local>
2018-01-04 15:15:09 -0500

    output to txt currently

--- node.js
+++ node.js
@@ -1,18 +1,5 @@
-// const exec = require('child_process').exec;
-// const testscript = exec('sh z2yt.sh /directory');
+const fs = require('fs');
+const { spawnSync } = require('child_process');
 
-// testscript.stdout.on('data', function(data){
-//     console.log(data); 
-
-//     // sendBackInfo();
-// });
-
-
-
-// testscript.stderr.on('data', function(data){
-//     console.log(data);
-//     // triggerErrorStuff(); 
-// });
-
-const child = require('child_process');
-child.spawn('sh', ['z2yt.sh'], {stdio: 'inherit'});
+fs.chmodSync('z2yt.sh', '755');
+const child = spawnSync('sh', ['z2yt.sh'], { stdio: 'inherit', });
--- z2yt.sh
+++ z2yt.sh
@@ -1,4 +1,34 @@
 #!/bin/sh
-echo "What would you like to name the video?"
+echo "\nEnter Title"
 read name
-echo "$name is the name"
+echo "title: $name" > ./vids/upload.txt
+echo "\nEnter Description"
+read description
+echo "description: $description" >> ./vids/upload.txt
+echo ""
+PS3='Enter Listing Choice: '
+options=("Public" "Private" "Unlisted")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Public")
+            echo "\n"
+            break
+            ;;
+        "Private")
+            echo "\n"
+            break
+            ;;
+        "Unlisted")
+            echo "\n"
+            break
+            ;;
+        *) echo invalid option;;
+    esac
+done
+echo "listing: $opt" >> ./vids/upload.txt
+
+
+# for i in ./vids/*.mp4; do
+#   mv "$i" ./vids/"$name.mp4"
+# done
